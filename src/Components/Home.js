d65d7ba303b43041600bc182cd59f0a117984dcf
arundhaas1 <arunram.9215@gmail.com>
2021-08-16 12:28:12 +0530

    add data

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import HomeHeader from './HomeHeader'
+
+function Home() {
+    return (
+        <div className="home">
+            <HomeHeader />
+        </div>
+    )
+}
+
+export default Home
---
+++ src/Components/HomeHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomeHeader.css";
+
+function HomeHeader() {
+  return (
+    <div className="homeheader">
+      i am header
+    </div>
+  );
+}
+
+export default HomeHeader;

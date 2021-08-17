63e3fb5bba063531b9af551b781ffce04287116a
arundhaas1 <arunram.9215@gmail.com>
2021-08-17 17:42:19 +0530

    add new page

--- src/Components/Addposts/Addposts.js
+++ src/Components/Addposts/Addposts.js
@@ -1,13 +1,43 @@
-import React from 'react'
-import './Addposts.css'
+import React from "react";
+import "./Addposts.css";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import CheckIcon from "@material-ui/icons/Check";
+import { Link } from "react-router-dom";
 
 function Addposts() {
-    return (
-        <div className="addposts">
-            <input type="file"></input>
+  return (
+    <div className="addposts">
+      <div className="addpostsheader">
+        <Link to="/">
+            <ArrowBackIcon style={{color:"black"}} fontSize="large" />
+        </Link>
+        <h2>New Post</h2>
+        <CheckIcon className="checkicon" fontSize="large" />
+      </div>
 
-        </div>
-    )
+      <div className="addpostsdata">
+        <img
+          alt=""
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvrwnDbtNcrI-dGYTymzFiIqCWWlLKxHpEew&usqp=CAU"
+        />
+        <input type="text" placeholder="Write a caption.."></input>
+        <label className="addimage">
+          <i className=" ">Add images +</i>
+          <input
+            id="input"
+            style={{ display: "none" }}
+            type="file"
+            accept=".jpg,.jpeg,.png"
+          />
+        </label>
+      </div>
+
+      <p className="tag">Tag People</p>
+
+      <p className="tag">Add Location</p>
+
+    </div>
+  );
 }
 
-export default Addposts
+export default Addposts;

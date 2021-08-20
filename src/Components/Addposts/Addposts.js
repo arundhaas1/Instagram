901e29bb5e0c61eb0055351886049790bc692a28
arundhaas1 <arunram.9215@gmail.com>
2021-08-20 10:17:19 +0530

    posts

--- src/Components/Addposts/Addposts.js
+++ src/Components/Addposts/Addposts.js
@@ -1,16 +1,18 @@
-import React,{useEffect, useState} from "react";
+import React, { useState } from "react";
 import "./Addposts.css";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import CheckIcon from "@material-ui/icons/Check";
 import { Link } from "react-router-dom";
 import db from "../Firebase.js";
 import { useDataLayerValue } from "../datalayer";
+import firebase from "firebase";
 
 function Addposts() {
-
-  const[input,setInput]=useState=("")
-  const[posts,setPosts]=useState("")
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState("");
   const [{ user }, dispatch] = useDataLayerValue();
+
+  console.log("in", input)
 
   // useEffect(() => {
   //   db.collection("InstagramPosts")
@@ -25,18 +27,22 @@ function Addposts() {
   const postIt = (e) => {
     e.preventDefault();
     db.collection("InstagramPosts").add({
-      // message: input,
+      caption: input,
       username: user.displayName,
+      image: user.photoURL,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
+
+    setInput("")
   };
   return (
     <div className="addposts">
       <div className="addpostsheader">
         <Link to="/">
-            <ArrowBackIcon style={{color:"black"}} fontSize="large" />
+          <ArrowBackIcon style={{ color: "black" }} fontSize="large" />
         </Link>
         <h2>New Post</h2>
-        <CheckIcon className="checkicon" fontSize="large" />
+        <CheckIcon onClick={postIt} className="checkicon" fontSize="large" />
       </div>
 
       <div className="addpostsdata">
@@ -44,7 +50,11 @@ function Addposts() {
           alt=""
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvrwnDbtNcrI-dGYTymzFiIqCWWlLKxHpEew&usqp=CAU"
         />
-        <input onChange={e=>setInput(e.target.value)} type="text" placeholder="Write a caption.."></input>
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          placeholder="Write a caption.."
+        ></input>
         <label className="addimage">
           <i className=" ">Add images +</i>
           <input
@@ -59,7 +69,6 @@ function Addposts() {
       <p className="tag">Tag People</p>
 
       <p className="tag">Add Location</p>
-
     </div>
   );
 }

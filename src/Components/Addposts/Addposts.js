99090afd5f04cc78826f3d37469ccc458d49c918
arundhaas1 <arunram.9215@gmail.com>
2021-08-20 08:51:20 +0530

    chat

--- src/Components/Addposts/Addposts.js
+++ src/Components/Addposts/Addposts.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import "./Addposts.css";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import CheckIcon from "@material-ui/icons/Check";
 import { Link } from "react-router-dom";
+import db from "../Firebase.js";
+import { useDataLayerValue } from "../datalayer";
 
 function Addposts() {
+
+  const[input,setInput]=useState=("")
+  const[posts,setPosts]=useState("")
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  // useEffect(() => {
+  //   db.collection("InstagramPosts")
+  //     .orderBy("timestamp", "desc")
+  //     .onSnapshot((snapshot) => {
+  //       setPosts(
+  //         snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+  //       );
+  //     });
+  // }, []);
+
+  const postIt = (e) => {
+    e.preventDefault();
+    db.collection("InstagramPosts").add({
+      // message: input,
+      username: user.displayName,
+    });
+  };
   return (
     <div className="addposts">
       <div className="addpostsheader">
@@ -20,7 +44,7 @@ function Addposts() {
           alt=""
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvrwnDbtNcrI-dGYTymzFiIqCWWlLKxHpEew&usqp=CAU"
         />
-        <input type="text" placeholder="Write a caption.."></input>
+        <input onChange={e=>setInput(e.target.value)} type="text" placeholder="Write a caption.."></input>
         <label className="addimage">
           <i className=" ">Add images +</i>
           <input
